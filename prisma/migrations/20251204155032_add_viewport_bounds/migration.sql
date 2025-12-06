999fe5c27a28c2f05d1a3f6229ed281d31891819
Luke Watson-Davies <lukew@cogapp.com>
2025-12-06 10:44:55 +0000

    move to postgres via Neon

--- prisma/migrations/20251204155032_add_viewport_bounds/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Annotation" ADD COLUMN "viewportHeight" REAL;
-ALTER TABLE "Annotation" ADD COLUMN "viewportWidth" REAL;
-ALTER TABLE "Annotation" ADD COLUMN "viewportX" REAL;
-ALTER TABLE "Annotation" ADD COLUMN "viewportY" REAL;
